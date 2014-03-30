2f77462a5c4e0902fb8e9be4eb587a480477f410
sam <smrgrace@gmail.com>
2014-03-30 07:33:09 +0100

    finished first lessson

---
+++ coding_along.py
@@ -0,0 +1,16 @@
+__author__ = 'sam'
+
+
+class Main:
+    max_width = 5
+    max_height = 5
+    character_alive = True
+    character_won = False
+    monster_awake = False
+    monster_awakened = False
+    monster_moves_per_turn = 2
+
+    def __init__(self):
+        pass
+
+monster = Main()
